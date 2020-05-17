74d246c4263c0b1326abfb59f082dfd7cacffd9e
llindemann <2153696+llindemann@users.noreply.github.com>
2020-05-17 11:53:04 +0200

    fix variables implicit export

--- multipass/launch-ansible-vm.sh
+++ multipass/launch-ansible-vm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set +a
+set -a
 FILE=../.env
 if [ -f "$FILE" ]; then
     . ../.env
@@ -8,6 +8,6 @@ else
     echo "Please copy ../.env.template to ../.env, then update .env"
     exit 0
 fi
-set -a
+set +a
 
 envsubst < ../cloud-init-ansible-roles.yml.tmpl | multipass launch --name k3s-vm --mem 4G --disk 40G --cpus 2 $multipass_distro_version --cloud-init -
